8839b2100066743679db142da8cfd7d572b34f4b
Ben McCarver <69656145+benbot2775@users.noreply.github.com>
2022-08-19 12:09:38 -0500

    Add files via upload

---
+++ my code.py
@@ -0,0 +1,12 @@
+animals = None
+_7BtextVariable_7D = None
+_7BlistVariable_7D = None
+
+
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+
+animals = ['dog', 'cat', 'elk']
+print(animals[0])
+print(animals[1])
+print(animals[2])
